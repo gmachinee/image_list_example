9822e6da00726287745523306d1002edc2cdd4d2
gmachinee <gmachinee@gmail.com>
2024-07-18 21:03:44 +0900

    [feat] SearchBar 구현

--- lib/SearchPage.dart
+++ lib/SearchPage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-/// 이미지 검색 페이지
+/// SearchPage는 검색 페이지의 뷰를 담당합니다.
 class SearchPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -22,14 +22,29 @@ class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Center(child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("SearchPage"),
-        ],
-      ),), backgroundColor: Colors.black, foregroundColor: Colors.white,),
+      appBar: AppBar(title: Center(child: _buildSearchBar()), backgroundColor: Colors.black,),
       body: Center(
       ),
+    );
+  }
+
+  /// 검색창을 생성합니다
+  Widget _buildSearchBar() {
+    return SearchBar(
+      trailing: const [Icon(Icons.search)],
+      constraints: const BoxConstraints(maxWidth: 300, minHeight: 36),
+      hintText: "검색어를 입력하세요",
+      textInputAction: TextInputAction.done,
+      onChanged: (value) {
+        debugPrint('input text: $value');
+      },
+      onSubmitted: (value) {
+        // 검색어 미입력 시 스낵바로 알림
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text("검색어를 입력하세요"),
+          duration: Duration(seconds: 2),
+        ));
+      },
     );
   }
 }
